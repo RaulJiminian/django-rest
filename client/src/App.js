aa5709663831c6741e13ddf31b4a7b970131ced7
Raul Jiminian <raul.e.jiminian@gmail.com>
2022-01-27 21:39:09 -0500

    front-end created

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Header from "./components/layout/Header";
+import Dashboard from "./components/leads/Dashboard";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="container">
+        <Dashboard />
+      </div>
+    </div>
+  );
 }
 
 export default App;
